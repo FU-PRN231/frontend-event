9bf43db37502c7cb960b0b9800292ea71ff4668b
QuanDH <hongquan.contact@gmail.com>
2024-06-09 11:28:39 +0700

    commit chage link api

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,4 @@
-// const baseUrl = "https://fpt-event.azurewebsites.net";
-const baseUrl = "https://localhost:5001";
-
+const baseUrl = "https://fpt-event.azurewebsites.net";
+// const baseUrl = "https://localhost:5001";
 
 export { baseUrl };
--- src/api/eventApi.js
+++ src/api/eventApi.js
@@ -2,19 +2,21 @@ import axios from "axios";
 import { baseUrl } from "./config";
 
 export const getAllEvent = async (pageNumber, pageSize) => {
-    try {
-      const res = await axios.get(`${baseUrl}/api/Event/get-all-event?pageNumber=${pageNumber}&pageSize=${pageSize}`);
-      return res.data;
-    } catch (err) {
-      return null;
-    }
-  };
-  
-  export const getEventById = async (id) => {
-    try {
-      const res = await axios.get(`${baseUrl}/api/Event/get-event-by-id/${id}`);
-      return res.data;
-    } catch (err) {
-      return null;
-    }
+  try {
+    const res = await axios.get(
+      `${baseUrl}/event/get-all-event?pageNumber=${pageNumber}&pageSize=${pageSize}`
+    );
+    return res.data;
+  } catch (err) {
+    return null;
   }
+};
+
+export const getEventById = async (id) => {
+  try {
+    const res = await axios.get(`${baseUrl}/event/get-event-by-id/${id}`);
+    return res.data;
+  } catch (err) {
+    return null;
+  }
+};
