d981a2683aab36536f1f9e6f61a69f0db7d62c25
QuanDH <hongquan.contact@gmail.com>
2024-06-26 22:59:45 +0700

    commit

---
+++ src/api/dashboardApi.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { baseUrl } from "./config";
+export const getOverAllReport = async (organizationId, timePeriod) => {
+  try {
+    const response = await axios.get(
+      `${baseUrl}/report/get-overall-report?organizationId=${organizationId}&timePeriod=${timePeriod}`
+    );
+    return response.data;
+  } catch (err) {
+    return null;
+  }
+};
+
+export const getEventDetailReport = async (id) => {
+  try {
+    const response = await axios.get(
+      `${baseUrl}/report/get-event-detail-report/${id}`
+    );
+    return response.data;
+  } catch (err) {
+    return null;
+  }
+};
