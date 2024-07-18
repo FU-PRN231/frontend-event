963a011d1e44ec9ebe08beeec11d4656dae2adab
AnBinh1703 <dan1314705@gmail.com>
2024-07-18 13:00:38 +0700

    mẻrge agian in Master

--- src/pages/Common/SurveyForm.jsx
+++ src/pages/Common/SurveyForm.jsx
@@ -1,70 +1,104 @@
-import { Card, message, Select, Space, Spin, Typography } from "antd";
+import {
+  Button,
+  Card,
+  DatePicker,
+  Form,
+  Input,
+  message,
+  Rate,
+  Select,
+  Space,
+  Typography,
+} from "antd";
+import moment from "moment-timezone";
 import React, { useEffect, useState } from "react";
-import { getAllSurveys, getSurveyById } from "../../api/surveyApi";
+import { useSelector } from "react-redux";
+import {
+  addAnswerToSurvey,
+  getAllSurveys,
+  getSurveyById,
+} from "../../api/surveyApi";
 
+const { Title, Text } = Typography;
 const { Option } = Select;
-const { Text, Title, Paragraph } = Typography;
 
-const ViewModalBySurveyID = ({ surveyId, visible, onClose }) => {
-  const [loading, setLoading] = useState(false);
-  const [surveyData, setSurveyData] = useState(null);
+const SurveyForm = () => {
+  const [form] = Form.useForm();
+  const user = useSelector((state) => state.user.user || {});
+  const [surveyData, setSurveyData] = useState({});
   const [surveys, setSurveys] = useState([]);
   const [selectedSurveyId, setSelectedSurveyId] = useState(null);
-
   useEffect(() => {
-    const fetchSurveys = async () => {
-      try {
-        const response = await getAllSurveys();
-        if (response?.isSuccess) {
-          setSurveys(response.result);
-        }
-      } catch (error) {
-        message.error("Failed to fetch surveys: " + error.message);
-      }
-    };
-
     fetchSurveys();
   }, []);
 
-  const fetchSurveyById = async (id) => {
-    setLoading(true);
+  const fetchSurveys = async () => {
     try {
-      const response = await getSurveyById(id);
+      const response = await getAllSurveys();
       if (response?.isSuccess) {
-        setSurveyData(response.result);
-      } else {
-        setSurveyData(null);
+        setSurveys(response.result);
       }
     } catch (error) {
+      message.error("Failed to fetch surveys: " + error.message);
+    }
+  };
+
+  const fetchSurveyById = async (id) => {
+    try {
+      const response = await getSurveyById(id);
+      setSurveyData(response.result);
+    } catch (error) {
       message.error("Failed to fetch survey details: " + error.message);
-    } finally {
-      setLoading(false);
     }
   };
 
   const handleSurveySelect = (value) => {
     setSelectedSurveyId(value);
     fetchSurveyById(value);
+    form.resetFields();
   };
 
-  const formatDate = (dateString) => {
-    if (!dateString) return "";
-    const date = new Date(dateString);
-    if (isNaN(date.getTime())) {
-      console.error(`Invalid date string: ${dateString}`);
-      return "";
-    }
-    return date.toLocaleDateString("en-GB");
-  };
-
-  const getAccountDetails = (account) => {
-    if (!account)
-      return { name: "Unknown", phoneNumber: "Unknown", email: "Unknown" };
-    return {
-      name: `${account.firstName} ${account.lastName}`,
-      phoneNumber: account.phoneNumber || "Unknown",
-      email: account.email || "Unknown",
+  const handleSubmit = async (values) => {
+    const formattedResponses = {
+      accountId: user.id,
+      surveyId: surveyData.survey.id,
+      answerDetails: Object.entries(values).map(([key, value]) => {
+        const question = surveyData.surveyQuestionDetails.find(
+          (q) => q.id === key
+        );
+        return {
+          surveyQuestionDetailId: key,
+          textAnswer:
+            question.answerType === 0 || question.answerType === 2
+              ? value
+              : null,
+          rating: question.answerType === 1 ? value : null,
+        };
+      }),
     };
+
+    console.log("Formatted responses:", formattedResponses);
+    const response = await addAnswerToSurvey(formattedResponses);
+    if (response?.isSuccess) {
+      message.success(
+        "Khảo sát đã được gửi đi, cảm ơn bạn đã tham gia khảo sát"
+      );
+    } else {
+      message.error("Có lỗi xảy ra. Vui lòng submit lại");
+    }
+  };
+
+  const renderQuestionInput = (question) => {
+    switch (question.answerType) {
+      case 0:
+        return <Input placeholder="Nhập câu trả lời của bạn" />;
+      case 1:
+        return <Rate count={question.ratingMax || 5} />;
+      case 2:
+        return <DatePicker style={{ width: "100%" }} format={"dd/MM/YYYY"} />;
+      default:
+        return <Input placeholder="Nhập câu trả lời của bạn" />;
+    }
   };
 
   return (
@@ -72,9 +106,8 @@ const ViewModalBySurveyID = ({ surveyId, visible, onClose }) => {
       <Space direction="vertical" size="large" style={{ width: "100%" }}>
         <Select
           style={{ width: "100%" }}
-          placeholder="Select a survey"
+          placeholder="Chọn khảo sát"
           onChange={handleSurveySelect}
-          value={selectedSurveyId}
         >
           {surveys.map((survey) => (
             <Option key={survey.survey.id} value={survey.survey.id}>
@@ -83,46 +116,58 @@ const ViewModalBySurveyID = ({ surveyId, visible, onClose }) => {
           ))}
         </Select>
 
-        {loading && <Spin />}
-
-        {surveyData && (
+        {selectedSurveyId && (
           <>
-            <Title level={4}>{surveyData.survey.name}</Title>
-            <Paragraph>
-              <Text strong>Created By:</Text>{" "}
-              {getAccountDetails(surveyData.survey.createByAccount).name}
-            </Paragraph>
-            <Paragraph>
-              <Text strong>Phone:</Text>{" "}
-              {getAccountDetails(surveyData.survey.createByAccount).phoneNumber}
-            </Paragraph>
-            <Paragraph>
-              <Text strong>Email:</Text>{" "}
-              {getAccountDetails(surveyData.survey.createByAccount).email}
-            </Paragraph>
-            <Paragraph>
-              <Text strong>Created on:</Text>{" "}
-              {formatDate(surveyData.survey.createDate)}
-            </Paragraph>
-            <Paragraph>
-              <Text strong>Updated on:</Text>{" "}
-              {formatDate(surveyData.survey.updateDate)}
-            </Paragraph>
+            <Title level={2}>{surveyData.survey?.name}</Title>
+            <Text type="secondary">
+              Được tạo vào ngày:
+              {moment(surveyData.survey?.createDate).format("MMMM D, YYYY")}
+            </Text>
 
-            <Title level={5}>Survey Questions</Title>
-            {surveyData.surveyQuestionDetails.map((question) => (
-              <Paragraph key={question.id}>
-                <Text strong>{question.question}</Text>
-                <br />
-                <Text type="secondary">
-                  Type:{" "}
-                  {question.answerType === 0
-                    ? "Text"
-                    : `Rating (Max: ${question.ratingMax})`}
-                </Text>
-                {/* Display responses for each question if available */}
-              </Paragraph>
-            ))}
+            <Form
+              form={form}
+              onFinish={handleSubmit}
+              layout="vertical"
+              className="mt-6"
+            >
+              <Space
+                direction="vertical"
+                size="large"
+                style={{ width: "100%" }}
+              >
+                {surveyData?.surveyQuestionDetails?.map((question) => (
+                  <Form.Item
+                    key={question.id}
+                    name={question.id}
+                    label={
+                      <Text strong>
+                        {question.no}. {question.question}
+                      </Text>
+                    }
+                    rules={[
+                      {
+                        required: true,
+                        message: "Please answer this question",
+                      },
+                    ]}
+                    getValueFromEvent={(e) => {
+                      if (question.answerType === 2) {
+                        // return e ? e.format("YYYY-MM-DD") : null;
+                      }
+                      return e && e.target ? e.target.value : e;
+                    }}
+                  >
+                    {renderQuestionInput(question)}
+                  </Form.Item>
+                ))}
+
+                <Form.Item>
+                  <Button type="primary" htmlType="submit" block>
+                    Gửi trả lời khảo sát
+                  </Button>
+                </Form.Item>
+              </Space>
+            </Form>
           </>
         )}
       </Space>
@@ -130,4 +175,4 @@ const ViewModalBySurveyID = ({ surveyId, visible, onClose }) => {
   );
 };
 
-export default ViewModalBySurveyID;
+export default SurveyForm;
